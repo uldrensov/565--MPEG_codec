10db12ff4f1ac529eedb2a5d7a5c5b80418d5c4a
Cliff <dracheeis@gmail.com>
2019-12-07 01:33:52 -0800

    1.01: top-level README added

---
+++ README_hw_565_4.m
@@ -0,0 +1,23 @@
+function void = README_hw_565_4()
+% COMPE565 HW 4
+% 07 Dec 2019
+% Cliff Phan
+% 821_099_130
+% dracheeis@gmail.com
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The entire assignment is called with the main function, which calls 3
+%   helper functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    hw_565_4();
+        %produces multiple figures, as well as command window outputs.
+        %command window displays PSNR and MSE for each reconstructed
+            %frame, as well as the average PSNR across all frames.
+        %output figures occur in the following order:
+            %I-FRAME
+            %VECTORS(P1), RESIDUAL(P1), P-FRAME 1
+            %VECTORS(P2), RESIDUAL(P2), P-FRAME 2
+            %VECTORS(P3), RESIDUAL(P3), P-FRAME 3
+            %VECTORS(P4), RESIDUAL(P4), P-FRAME 4
+end
